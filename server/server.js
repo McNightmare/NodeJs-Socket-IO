41479a6f6acca7ae860026d49b7998d8c9338141
McNightmare <ricky.vandenberg@hotmail.nl>
2017-01-14 19:23:18 +0100

    refactored message sending

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -16,26 +17,14 @@ io.on('connection', (socket) => {
   clients++;
   console.log(`User connected, ${clients} Users(s) online`);
 
-  socket.broadcast.emit('joinBroadcast', 'a user joined the room');
+  socket.broadcast.emit('newMessage',
+  generateMessage('Admin', 'A user joined the room'));
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
-
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'A user has joined the chatroom',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage',
+    generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.on('createMessage', (newMessage) => {
-    io.emit('newMessage', {
-      from: newMessage.from,
-      text: newMessage.text,
-      createdAt: new Date().getTime()
-    })
+    io.emit('newMessage', generateMessage(message.from, message.text))
   })
 
   socket.on('disconnect', () => {
